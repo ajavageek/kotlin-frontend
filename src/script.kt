0316bfb19d375f5dc626466f918fce5f14d0446a
Nicolas Frankel <nicolas@frankel.ch>
2017-04-03 19:07:18 +0200

    Improve Google Maps integration

--- src/script.kt
+++ src/script.kt
@@ -1,4 +1,5 @@
 import google.maps.GoogleMap
+import org.w3c.dom.Element
 import kotlin.browser.document
 import kotlin.js.json
 
@@ -9,9 +10,11 @@ internal class LatitudeLongitude(val latitude: Double, val longitude: Double)
 internal fun LatitudeLongitude.toJson() = json("lat" to latitude, "lng" to longitude)
 internal fun MapOptions.toJson() = json("center" to center.toJson(), "zoom" to zoom)
 
+internal class KotlinGoogleMap(element: Element?, options: MapOptions) : GoogleMap(element, options.toJson())
+
 fun initMap() {
     val div = document.getElementById("map")
     val latLng = LatitudeLongitude(latitude = -34.397, longitude = 150.644)
     val options = MapOptions(center = latLng, zoom = 8)
-    GoogleMap(element = div, options = options.toJson())
+    KotlinGoogleMap(element = div, options = options)
 }
